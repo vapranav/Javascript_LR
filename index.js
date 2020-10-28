b51efa1050319d0f73e2d820bb862e7fe89443f0
vapranav <pranavva34@gmail.com>
2020-10-28 18:12:41 +0530

    tweak train data

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const port = 3000
 const MLR = require("ml-regression-multivariate-linear");
 
 const x = [
-  [0],
-  [1],
-  [2],
-  [3]
+  [100, 250],
+  [200, 300],
+  [300, 230],
+  [400, 100],
 ];
 // Y0 = X0 * 2, Y1 = X1 * 2, Y2 = X0 + X1
 const y = [
-  [0],
-  [2],
-  [4],
-  [6]
+  [10000],
+  [11500],
+  [12000],
+  [15000]
 ];
 
 const mlr = new MLR(x, y);
 
-app.post('/:numparticipants', (req, res) => {
+app.post('/:numparticipants/:price', (req, res) => {
       let a = new Array;
       a.push(parseInt(req.params.numparticipants));
+      a.push(parseInt(req.params.price));
       res.json(mlr.predict(a));
 })
 
